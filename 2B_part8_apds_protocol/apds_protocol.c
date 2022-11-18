087382f0eaab84165ce93397a2e9df2efd036113
skyfall88888 <95589555+skyfall88888@users.noreply.github.com>
2022-11-18 18:58:47 -0500

    Update apds_protocol.c

--- 2B_part8_apds_protocol/apds_protocol.c
+++ 2B_part8_apds_protocol/apds_protocol.c
@@ -3,35 +3,44 @@
 #include <stdlib.h>
 
 #include "pico/stdlib.h"
-
 #include "hardware/pio.h"
-
+#include "neopixel.h"
 #include "pio_i2c.h"
 
 #define PIN_SDA 22
 #define PIN_SCL 23
+#define WS2812_POWER_PIN 11
+#define WS2812_PIN 12
 const int addr = 0x39;
 uint8_t clear_low = 0x94;
 uint8_t clear_high = 0x95;
-const uint8_t red_low = 0x96;
-const uint8_t red_high = 0x97;
+uint8_t red_low = 0x96;
+uint8_t red_high = 0x97;
 const uint8_t green_low = 0x98;
 const uint8_t green_high = 0x99;
 const uint8_t blue_low = 0x9A;
 const uint8_t blue_high = 0x9B;
-uint8_t PDATA = 0x9C;
+uint8_t PDATA = 0x9c;
 uint8_t ATIME = 0x81;
 
 
 const uint8_t ALS_TIME = 219;
-const uint8_t pon_addr = 0x80;
-const uint8_t pon_val = 0b00000111;
+const uint8_t enable_addr = 0x80;
+// const uint8_t pon_val = 0b00000111;
 
 const uint8_t gmode_addr = 0xAB;
-const uint8_t gmode_val = 0b00000001;
+const uint8_t gmode_val = 0b00000000;
 
-const uint8_t sai_addr = 0x9F;
-const uint8_t sai_val = 0b00000000;
+// const uint8_t sai_addr = 0x9F;
+// const uint8_t sai_val = 0b00000000;
+
+
+#define GEN 0 //gesture enable 
+#define AEN 1 //ALS Enable. 
+#define PON 1 //Power ON. 
+#define PEN 0 //Proximity enable.
+
+#define CONFIGURATION (GEN << 6U) | (PEN << 2u) | (AEN << 1u) | PON
 
 uint16_t buffer_c;
 uint16_t buffer_r;
@@ -44,17 +53,18 @@ int returned;
 
 
 PIO pio = pio0;
-uint sm = 0;
+uint sm = 1;
 
 bool reserved_addr(uint8_t addr) {
     return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
 }
 
+
 int apds_init() {
 
-    buf[0] = pon_addr;
-    buf[1] = pon_val;
-    returned = pio_i2c_write_blocking(pio, sm, addr, buf, 2); // true to keep master control of bus
+    buf[0] = enable_addr;
+    buf[1] = CONFIGURATION;
+    returned = pio_i2c_write_blocking(pio, sm, addr, buf, 2, false); // true to keep master control of bus
     if (returned ==  PICO_ERROR_GENERIC) {
         printf("ERROR: PON not written\n");
         return -1;
@@ -63,13 +73,13 @@ int apds_init() {
 
     buf[0] = ATIME;
     buf[1] = ALS_TIME;
-    pio_i2c_write_blocking(pio, sm, addr, buf, 2);
+    pio_i2c_write_blocking(pio, sm, addr, buf, 2, false);
 
-    // // config gmode
+    // config gmode
     // //printf("wite to gmode\n");
     // buf[0] = gmode_addr;
-    // buf[1] = gmode_val;
-    // returned = pio_i2c_write_blocking(pio, sm, addr, buf, 2); // true to keep master control of bus
+    // buf[1] = gmode_val;  //gmode = 0 at first
+    // returned = pio_i2c_write_blocking(pio, sm, addr, buf, 2,false); // true to keep master control of bus
     // if (returned ==  PICO_ERROR_GENERIC) {
     //     printf("ERROR: GMODE not written\n");
     //     return -2;
@@ -79,52 +89,43 @@ int apds_init() {
     
 }
 
-static void read_raw() {
-    uint8_t lsb;
-    uint8_t msb;
+static void read_prox() {
     uint8_t proxi;
 
-    // printf("read from buffer-c\n");
-    pio_i2c_write_blocking(pio, sm, addr, &PDATA, 1);
-    pio_i2c_read_blocking(pio, sm, addr, &proxi, 1);
-
-
-    pio_i2c_write_blocking(pio, sm, addr, &clear_low ,1);
-    pio_i2c_read_blocking(pio, sm, addr, &lsb, 1);
-
-
-    pio_i2c_write_blocking(pio, sm, addr, &clear_high, 1);
-    pio_i2c_read_blocking(pio, sm, addr, &msb, 1);
+    printf("read from buffer-c\n");
+    pio_i2c_write_blocking(pio, sm, addr, &PDATA, 1,true);
+    pio_i2c_read_blocking(pio, sm, addr, &proxi, 1, false);
 
     printf("distance: %u\n", proxi);
-    printf("msb: %u\n", msb);
-    printf("lsb: %u\n", lsb);
-
-    buffer_c = (msb << 8) | lsb;
-    //i2c_read_blocking(i2c_default, clear_high, (buffer_c+4), 8, true);
-
-    // printf("read from buffer-r\n");
-    // i2c_read_blocking(i2c_default, red_low, buffer_r, 8, true);
-    // i2c_read_blocking(i2c_default, red_high, (buffer_r+4), 8, true);
-
-    // printf("read from buffer_g\n");
-    // i2c_read_blocking(i2c_default, green_low, buffer_g, 8, true);
-    // i2c_read_blocking(i2c_default, green_high, (buffer_g+4), 8, true);
-
-    // printf("read from buffer_b\n");
-    // i2c_read_blocking(i2c_default, blue_low, buffer_b, 8, true);
-    // i2c_read_blocking(i2c_default, blue_high, (buffer_b+4), 8, true);
-    // buf[0] = sai_addr;
-    // buf[1] = sai_val;
-
-    // pio_i2c_write_blocking(pio,sm, addr, buf, 2);
-
-    //}
+   
 }
 
+void read_rgbc(int32_t* r, int32_t* g, int32_t* b, int32_t* c) {
 
+    uint8_t buf[8];
+    uint8_t reg = clear_low;
+    returned = pio_i2c_write_blocking(pio, sm, addr, &reg, 1, true);  // true to keep master control of bus
+    // printf("write rgbc status: %d\n", returned);
+    returned = pio_i2c_read_blocking(pio, sm, addr, buf, 8, false);  // false - finished with bus
+    // printf("read rgbc status: %d\n", returned);
+
+
+    *c = (buf[1] << 8) | buf[0];
+    *r = (buf[3] << 8) | buf[2];
+    *g = (buf[5] << 8) | buf[4];
+    *b = (buf[7] << 8) | buf[6];
+}
+
+static inline uint32_t rgbc_u32(uint8_t r, uint8_t g, uint8_t b) {
+    return
+            ((uint32_t) (r) << 8) |
+            ((uint32_t) (g) << 16) |
+            (uint32_t) (b);
+}
 
 int main() {
+    int32_t r,g,b,c;
+    uint32_t color;
 
     stdio_init_all();
 
@@ -148,7 +149,7 @@ int main() {
         if (reserved_addr(addr))
             result = -1;
         else
-            result = pio_i2c_read_blocking(pio, sm, addr, NULL, 0);
+            result = pio_i2c_read_blocking(pio, sm, addr, NULL, 0, false);
 
         printf(result < 0 ? "." : "@");
         printf(addr % 16 == 15 ? "\n" : "  ");
@@ -167,20 +168,21 @@ int main() {
     }
     
 
-    while (returned > 0) {
+    neopixel_init();
+    while (returned >= 0) {
         // printf("entered while\n");
-        read_raw();
+        // read_raw();
         // printf("back from get_raw\n");
-        // print_buffer(buffer_c);
-        // print_buffer(buffer_r);
-        // print_buffer(buffer_g);
-        // print_buffer(buffer_b);
-        printf("clear: %u\n", buffer_c);
+        // print_buffer
+        read_rgbc(&r, &g, &b, &c);
+        printf("r:%d, g:%d, b:%d, c:%d\n", r, g, b, c);
 
-        // printf("red: %u\n", buffer_r);
-        // printf("green: %u\n", buffer_g);
-        // printf("blue: %u\n", buffer_b);
-        sleep_ms(100);
+        color = rgbc_u32((uint8_t)r, (uint8_t)g,(uint8_t)b);
+
+        gpio_put(WS2812_POWER_PIN, true);
+        gpio_put(WS2812_PIN, true);
+        neopixel_set_rgb(color);
+        sleep_ms(3000);
     }
 
     return 0;
