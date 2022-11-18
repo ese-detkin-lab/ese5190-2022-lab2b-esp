1af762ed9eab0e229eb1b6a743bcc7218a2b1c7d
Aurunima <112366204+Aurunima@users.noreply.github.com>
2022-11-18 10:39:03 -0500

    Update main.c

--- lab/09_lab_on_a_chip/main.c
+++ lab/09_lab_on_a_chip/main.c
@@ -1,1 +1,389 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+#include "hardware/structs/bus_ctrl.h"
+#include "pico/multicore.h"
+#include "APDS9960.h"
+
+const uint CAPTURE_PIN_BASE = 12;
+const uint CAPTURE_PIN_COUNT = 1;
+const uint CAPTURE_N_SAMPLES = 350000;
+const uint TRIGGER_PIN = 21;
+
+#define PIN_SDA 22
+#define PIN_SCL 23
+
+
+const int PIO_I2C_ICOUNT_LSB = 10;
+const int PIO_I2C_FINAL_LSB  = 9;
+const int PIO_I2C_DATA_LSB   = 1;
+const int PIO_I2C_NAK_LSB    = 0;
+
+#define PIO         pio0
+#define SM          0
+#define FREQ        800000
+#define PIN         11
+#define POWER_PIN   12
+#define IS_RGBW     true  
+
+void neopixel_init() {
+    // power up WS2812 on Qt Py, using pin defs from board header file
+    // pico-sdk/src/boards/include/boards/adafruit_qtpy_rp2040.h
+    gpio_init(POWER_PIN);
+    gpio_set_dir(POWER_PIN, GPIO_OUT);
+    gpio_put(POWER_PIN, 1);
+
+    // program PIO using helper function from ws2812.pio
+    uint offset = pio_add_program(PIO, &ws2812_program);
+    ws2812_program_init(PIO, SM, offset, PIN, FREQ, IS_RGBW);
+}
+           
+
+void neopixel_set_rgb(uint32_t rgb) {
+    // convert RGB to GRB
+    uint32_t grb = ((rgb & 0xFF0000) >> 8) | ((rgb & 0x00FF00) << 8) | (rgb & 0x0000FF);
+    pio_sm_put_blocking(pio0, 0, grb << 8u);
+}
+
+#define POWER_ENABLE 1
+#define PROXIMITY_ENABLE 1
+#define ALS_ENABLE 1
+#define GESTURE_ENABLE 0
+
+#define ALS_GAIN 0
+#define ALS_TIME 219
+
+#define INIT_CONFIG (GESTURE_ENABLE << 6u) | (PROXIMITY_ENABLE << 2u) | (ALS_ENABLE << 1u) | POWER_ENABLE
+
+const int addr = 0x39;
+const int max_read = 250;
+
+#define PIN_SDA 22
+#define PIN_SCL 23
+
+
+
+void APDS9960_init(PIO pio, uint sm) {
+    // use the "handheld device dynamic" optimal setting (see datasheet)
+    uint8_t buf[2];
+
+    // send register number followed by its corresponding value
+    buf[0] = ENABLE_REG;
+    buf[1] = INIT_CONFIG;
+    pio_i2c_write_blocking(pio, sm, addr, buf, 2, false);
+
+    buf[0] = ALS_TIME_REG;
+    buf[1] = ALS_TIME;
+    pio_i2c_write_blocking(pio, sm, addr, buf, 2, false);
+}
+
+void read_proximity(PIO pio, uint sm, int32_t* proximity) {
+
+    uint8_t buf[1];
+    uint8_t reg = PDATA_REG;
+    pio_i2c_write_blocking(pio, sm, addr, &reg, 1, true);  // true to keep master control of bus
+    pio_i2c_read_blocking(pio, sm, addr, buf, 1, false);  // false - finished with bus
+
+    *proximity = buf[0];
+}
+
+void read_rgbc(PIO pio, uint sm, int32_t* r, int32_t* g, int32_t* b, int32_t* c) {
+
+    uint8_t buf[2];
+    uint8_t reg = CDATA_REG_L;
+    pio_i2c_write_blocking(pio, sm, addr, &reg, 1, true);  // true to keep master control of bus
+    pio_i2c_read_blocking(pio, sm, addr, buf, 8, false);  // false - finished with bus
+
+    *c = (buf[1] << 8) | buf[0];
+    *r = (buf[3] << 8) | buf[2];
+    *g = (buf[5] << 8) | buf[4];
+    *b = (buf[7] << 8) | buf[6];
+}
+
+bool pio_i2c_check_error(PIO pio, uint sm) {
+    return pio_interrupt_get(pio, sm);
+}
+
+void pio_i2c_resume_after_error(PIO pio, uint sm) {
+    pio_sm_drain_tx_fifo(pio, sm);
+    pio_sm_exec(pio, sm, (pio->sm[sm].execctrl & PIO_SM0_EXECCTRL_WRAP_BOTTOM_BITS) >> PIO_SM0_EXECCTRL_WRAP_BOTTOM_LSB);
+    pio_interrupt_clear(pio, sm);
+}
+
+void pio_i2c_rx_enable(PIO pio, uint sm, bool en) {
+    if (en)
+        hw_set_bits(&pio->sm[sm].shiftctrl, PIO_SM0_SHIFTCTRL_AUTOPUSH_BITS);
+    else
+        hw_clear_bits(&pio->sm[sm].shiftctrl, PIO_SM0_SHIFTCTRL_AUTOPUSH_BITS);
+}
+
+static inline void pio_i2c_put16(PIO pio, uint sm, uint16_t data) {
+    while (pio_sm_is_tx_fifo_full(pio, sm))
+        ;
+    // some versions of GCC dislike this
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+    *(io_rw_16 *)&pio->txf[sm] = data;
+#pragma GCC diagnostic pop
+}
+
+
+// If I2C is ok, block and push data. Otherwise fall straight through.
+void pio_i2c_put_or_err(PIO pio, uint sm, uint16_t data) {
+    while (pio_sm_is_tx_fifo_full(pio, sm))
+        if (pio_i2c_check_error(pio, sm))
+            return;
+    if (pio_i2c_check_error(pio, sm))
+        return;
+    // some versions of GCC dislike this
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+    *(io_rw_16 *)&pio->txf[sm] = data;
+#pragma GCC diagnostic pop
+}
+
+uint8_t pio_i2c_get(PIO pio, uint sm) {
+    return (uint8_t)pio_sm_get(pio, sm);
+}
+
+void pio_i2c_start(PIO pio, uint sm) {
+    pio_i2c_put_or_err(pio, sm, 1u << PIO_I2C_ICOUNT_LSB); // Escape code for 2 instruction sequence
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD0]);    // We are already in idle state, just pull SDA low
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC0_SD0]);    // Also pull clock low so we can present data
+}
+
+void pio_i2c_stop(PIO pio, uint sm) {
+    pio_i2c_put_or_err(pio, sm, 2u << PIO_I2C_ICOUNT_LSB);
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC0_SD0]);    // SDA is unknown; pull it down
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD0]);    // Release clock
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD1]);    // Release SDA to return to idle state
+};
+
+void pio_i2c_repstart(PIO pio, uint sm) {
+    pio_i2c_put_or_err(pio, sm, 3u << PIO_I2C_ICOUNT_LSB);
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC0_SD1]);
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD1]);
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD0]);
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC0_SD0]);
+}
+
+static void pio_i2c_wait_idle(PIO pio, uint sm) {
+    // Finished when TX runs dry or SM hits an IRQ
+    pio->fdebug = 1u << (PIO_FDEBUG_TXSTALL_LSB + sm);
+    while (!(pio->fdebug & 1u << (PIO_FDEBUG_TXSTALL_LSB + sm) || pio_i2c_check_error(pio, sm)))
+        tight_loop_contents();
+}
+
+int pio_i2c_write_blocking(PIO pio, uint sm, uint8_t addr, uint8_t *txbuf, uint len, bool nostop) {
+    int err = 0;
+    pio_i2c_start(pio, sm);
+    pio_i2c_rx_enable(pio, sm, false);
+    pio_i2c_put16(pio, sm, (addr << 2) | 1u);
+    while (len && !pio_i2c_check_error(pio, sm)) {
+        if (!pio_sm_is_tx_fifo_full(pio, sm)) {
+            --len;
+            pio_i2c_put_or_err(pio, sm, (*txbuf++ << PIO_I2C_DATA_LSB) | ((len == 0) << PIO_I2C_FINAL_LSB) | 1u);
+        }
+    }
+    if (!nostop)
+        pio_i2c_stop(pio, sm);
+    pio_i2c_wait_idle(pio, sm);
+    if (pio_i2c_check_error(pio, sm)) {
+        err = -1;
+        pio_i2c_resume_after_error(pio, sm);
+        pio_i2c_stop(pio, sm);
+    }
+    return err;
+}
+
+int pio_i2c_read_blocking(PIO pio, uint sm, uint8_t addr, uint8_t *rxbuf, uint len, bool nostop) {
+    int err = 0;
+    pio_i2c_repstart(pio, sm);
+    pio_i2c_rx_enable(pio, sm, true);
+    while (!pio_sm_is_rx_fifo_empty(pio, sm))
+        (void)pio_i2c_get(pio, sm);
+    pio_i2c_put16(pio, sm, (addr << 2) | 3u);
+    uint32_t tx_remain = len; // Need to stuff 0xff bytes in to get clocks
+
+    bool first = true;
+
+    while ((tx_remain || len) && !pio_i2c_check_error(pio, sm)) {
+        if (tx_remain && !pio_sm_is_tx_fifo_full(pio, sm)) {
+            --tx_remain;
+            pio_i2c_put16(pio, sm, (0xffu << 1) | (tx_remain ? 0 : (1u << PIO_I2C_FINAL_LSB) | (1u << PIO_I2C_NAK_LSB)));
+        }
+        if (!pio_sm_is_rx_fifo_empty(pio, sm)) {
+            if (first) {
+                // Ignore returned address byte
+                (void)pio_i2c_get(pio, sm);
+                first = false;
+            }
+            else {
+                --len;
+                *rxbuf++ = pio_i2c_get(pio, sm);
+            }
+        }
+    }
+    pio_i2c_stop(pio, sm);
+    pio_i2c_wait_idle(pio, sm);
+    if (pio_i2c_check_error(pio, sm)) {
+        err = -1;
+        pio_i2c_resume_after_error(pio, sm);
+        pio_i2c_stop(pio, sm);
+    }
+    return err;
+}
+
+void logic_analyser_init(PIO pio, uint sm, uint pin_base, uint pin_count, float div) {
+    // Load a program to capture n pins. This is just a single `in pins, n`
+    // instruction with a wrap.
+    uint16_t capture_prog_instr = pio_encode_in(pio_pins, pin_count);
+    struct pio_program capture_prog = {
+            .instructions = &capture_prog_instr,
+            .length = 1,
+            .origin = -1
+    };
+    uint offset = pio_add_program(pio, &capture_prog);
+
+    // Configure state machine to loop over this `in` instruction forever,
+    // with autopush enabled.
+    pio_sm_config c = pio_get_default_sm_config();
+    sm_config_set_in_pins(&c, pin_base);
+    sm_config_set_wrap(&c, offset, offset);
+    sm_config_set_clkdiv(&c, div);
+    // Note that we may push at a < 32 bit threshold if pin_count does not
+    // divide 32. We are using shift-to-right, so the sample data ends up
+    // left-justified in the FIFO in this case, with some zeroes at the LSBs.
+    sm_config_set_in_shift(&c, true, true, bits_packed_per_word(pin_count));
+    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_RX);
+    pio_sm_init(pio, sm, offset, &c);
+}
+
+void logic_analyser_arm(PIO pio, uint sm, uint dma_chan, uint32_t *capture_buf, size_t capture_size_words,
+                        uint trigger_pin, bool trigger_level) {
+    pio_sm_set_enabled(pio, sm, false);
+    // Need to clear _input shift counter_, as well as FIFO, because there may be
+    // partial ISR contents left over from a previous run. sm_restart does this.
+    pio_sm_clear_fifos(pio, sm);
+    pio_sm_restart(pio, sm);
+
+    dma_channel_config c = dma_channel_get_default_config(dma_chan);
+    channel_config_set_read_increment(&c, false);
+    channel_config_set_write_increment(&c, true);
+    channel_config_set_dreq(&c, pio_get_dreq(pio, sm, false));
+
+    dma_channel_configure(dma_chan, &c,
+        capture_buf,        // Destination pointer
+        &pio->rxf[sm],      // Source pointer
+        capture_size_words, // Number of transfers
+        true                // Start immediately
+    );
+
+    pio_sm_exec(pio, sm, pio_encode_wait_gpio(trigger_level, trigger_pin));
+    pio_sm_set_enabled(pio, sm, true);
+}
+
+
+void print_capture_buf(const uint32_t *buf, uint pin_base, uint pin_count, uint32_t n_samples) {
+    // Display the capture buffer in text form, like this:
+    // 00: __--__--__--__--__--__--
+    // 01: ____----____----____----
+    printf("Capture:\n");
+    // Each FIFO record may be only partially filled with bits, depending on
+    // whether pin_count is a factor of 32.
+    uint record_size_bits = bits_packed_per_word(pin_count);
+    for (int pin = 0; pin < pin_count; ++pin) {
+        printf("%02d: ", pin + pin_base);
+        for (int sample = 0; sample < n_samples; ++sample) {
+            uint bit_index = pin + sample * pin_count;
+            uint word_index = bit_index / record_size_bits;
+            // Data is left-justified in each FIFO entry, hence the (32 - record_size_bits) offset
+            uint word_mask = 1u << (bit_index % record_size_bits + 32 - record_size_bits);
+            //printf(buf[word_index] & word_mask ? "-" : "_");
+            printf(buf[word_index] & word_mask ? "1" : "0");
+        }
+        printf("\n");
+    }
+}
+
+//core 1 is set to keep fetching data from APDS9960
+void core1_main() {
+    int32_t proximity;
+    int32_t r, g, b, c;
+    PIO pio_i2c = pio1;
+    uint sm = 0;
+    while(true){
+        read_proximity(pio_i2c, sm, &proximity);
+        read_rgbc(pio_i2c, sm, &r, &g, &b, &c);
+        printf("proximity: %d   ",proximity);
+        printf("r:%d, g:%d, b:%d, c:%d\n", r, g, b, c);
+        neopixel_set_rgb((r << 16u | g << 8u | b));
+        sleep_ms(5);
+    }
+}
+
+int main() {
+    stdio_init_all();
+
+    // We're going to capture into a u32 buffer, for best DMA efficiency. Need
+    // to be careful of rounding in case the number of pins being sampled
+    // isn't a power of 2.
+    uint total_sample_bits = CAPTURE_N_SAMPLES * CAPTURE_PIN_COUNT;
+    total_sample_bits += bits_packed_per_word(CAPTURE_PIN_COUNT) - 1;
+    uint buf_size_words = total_sample_bits / bits_packed_per_word(CAPTURE_PIN_COUNT);
+    uint32_t * capture_buf = malloc(buf_size_words * sizeof(uint32_t));
+    hard_assert(capture_buf);
+
+    // Grant high bus priority to the DMA, so it can shove the processors out
+    // of the way. This should only be needed if you are pushing things up to
+    // >16bits/clk here, i.e. if you need to saturate the bus completely.
+    bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
+
+    //pio and DMA setting
+    PIO pio = pio0;
+    PIO pio_i2c = pio1;
+    uint sm = 0;
+    uint dma_chan = 0;
+
+    //-------------------------initialization--------------------------
+
+    logic_analyser_init(pio, sm, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT, 125000000 / (4 * 400 * 8 * 1000));// double freq of i2c rate. 400kbits/s => 800*8kHz
+
+    gpio_init(TRIGGER_PIN);
+    gpio_set_dir(TRIGGER_PIN, GPIO_IN);
+    sleep_ms(5000);
+
+    // initialize PIO.I2C and APDS9960
+    uint offset = pio_add_program(pio_i2c, &i2c_program);
+    i2c_program_init(pio_i2c, sm, offset, PIN_SDA, PIN_SCL);
+    APDS9960_init(pio_i2c, sm);// the default i2c rate is set to 400kHz
+
+    neopixel_init();
+    
+    multicore_launch_core1(core1_main); //keep fetching data from APDS9960 through core1.
+
+    //-------------------------initialization--------------------------
+
+    while(true){   
+        printf("press boot button to arming trigger\n");
+
+        do{} while (gpio_get(TRIGGER_PIN) == 1);
+
+        logic_analyser_arm(pio, sm, dma_chan, capture_buf, buf_size_words, TRIGGER_PIN, false);
+
+        printf("Start recording\n");
+        
+        dma_channel_wait_for_finish_blocking(dma_chan);
+        printf("Recording done!\n");
+
+        print_capture_buf(capture_buf, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT, CAPTURE_N_SAMPLES);
+
+        sleep_ms(1000);
+        }
+        
+}
