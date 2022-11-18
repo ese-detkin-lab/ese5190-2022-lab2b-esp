ac70ac009cd3005720a79ed7cc419cf6cb1ffcf0
Aurunima <112366204+Aurunima@users.noreply.github.com>
2022-11-18 10:45:15 -0500

    Update flashlight.c

--- code/flashlight/flashlight.c
+++ code/flashlight/flashlight.c
@@ -1,1 +1,105 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include "hardware/gpio.h"
 
+#define QTPY_BOOT_PIN 21
+
+#define PIO         pio0
+#define SM          0
+#define FREQ        800000
+#define PIN         11
+#define POWER_PIN   12
+#define IS_RGBW     true  
+
+void neopixel_init() {
+    // power up WS2812 on Qt Py, using pin defs from board header file
+    // pico-sdk/src/boards/include/boards/adafruit_qtpy_rp2040.h
+    gpio_init(POWER_PIN);
+    gpio_set_dir(POWER_PIN, GPIO_OUT);
+    gpio_put(POWER_PIN, 1);
+
+    // program PIO using helper function from ws2812.pio
+    uint offset = pio_add_program(PIO, &ws2812_program);
+    ws2812_program_init(PIO, SM, offset, PIN, FREQ, IS_RGBW);
+}
+           
+
+void neopixel_set_rgb(uint32_t rgb) {
+    // convert RGB to GRB
+    uint32_t grb = ((rgb & 0xFF0000) >> 8) | ((rgb & 0x00FF00) << 8) | (rgb & 0x0000FF);
+    pio_sm_put_blocking(pio0, 0, grb << 8u);
+}
+
+volatile uint32_t* boot_pin_address;
+uint32_t full_gpio_register_value;
+uint32_t pin_21_selection_mask;
+uint32_t select_pin_state;
+uint32_t shifted_pin_21_state;
+
+typedef struct {
+    uint32_t last_serial_byte;
+    uint32_t button_is_pressed;
+    uint32_t light_color;
+} Flashlight; 
+
+void render_to_console(Flashlight status) {
+    // adjust console window height to match 'frame size'
+    for (uint8_t i=0; i<10; i++) { 
+        printf("\n");
+    }
+    printf("button_is_pressed:  0x%08x\n",   status.button_is_pressed);
+    printf("light_color:        0x%08x\n",   status.light_color);
+}  
+
+int main() {
+
+    stdio_init_all();
+    gpio_init(QTPY_BOOT_PIN);
+    gpio_set_dir(QTPY_BOOT_PIN, GPIO_IN);
+    neopixel_init();
+
+    Flashlight status;
+    status.last_serial_byte =  0x00000000;
+    status.button_is_pressed = 0x00000000;
+    status.light_color =       0x000000ff;
+
+    while (true) {
+        status.last_serial_byte = getchar_timeout_us(0); // don't block main loop
+        switch(status.last_serial_byte) { // poll every cycle
+            case 'r':
+                status.light_color = 0x00ff0000;
+                break;
+            case 'g':
+                status.light_color = 0x0000ff00;
+                break;
+            case 'b':
+                status.light_color = 0x000000ff;
+                break;
+            case 'w':
+                status.light_color = 0x00ffffff;
+                break;
+        }
+
+        boot_pin_address = (volatile uint32_t*)0xd0000004;
+        full_gpio_register_value = (uint32_t)*boot_pin_address;
+        pin_21_selection_mask = 1u << 21;
+        select_pin_state = full_gpio_register_value & pin_21_selection_mask;
+        shifted_pin_21_state = select_pin_state >> 21;
+
+        if (shifted_pin_21_state) { // poll every cycle, 0 = "pressed"
+            status.button_is_pressed = 0x00000000;
+        }
+        else {
+            status.button_is_pressed = 0x00000001;
+        }
+      
+        if (status.button_is_pressed) { // poll every cycle
+            neopixel_set_rgb(status.light_color);
+        } else {
+            neopixel_set_rgb(0x00000000);
+        }
+        render_to_console(status);
+        sleep_ms(10); // don't DDOS the serial console
+    }
+    return 0;
+}     
