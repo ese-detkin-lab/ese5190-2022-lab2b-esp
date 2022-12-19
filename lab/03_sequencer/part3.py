5a8ed5b94e12e4ec89363afb545e18ddd88c8754
Praveen-Raj-u-s <114270637+Praveen-Raj-u-s@users.noreply.github.com>
2022-12-18 19:24:44 -0500

    Add files via upload

---
+++ lab/03_sequencer/part3.py
@@ -0,0 +1,22 @@
+import serial
+import time
+import csv
+import numpy
+import neopixel
+
+ser=serial.Serial('COM4', 115200)
+log = numpy.zeros((1001, 1))
+n=0
+
+file = open('ButtonLog.csv', 'w', newline='')
+writer = csv.writer(file)
+
+while n < 1000: #time.time() <= t_end:
+    line = ser.readline()
+    #log[n,0] = chr(line) #line[9]
+    n=n+1
+    writer.writerow(chr(line)) #line[9]
+    time.sleep(.01)
+    if n == 1001:
+        print(log)
+        file.close()
